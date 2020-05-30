b680cc42a9a7be01770e119870a9ab1738b0d5a7
Alex Benavidez Alvarado <alexbenavidezalvarado@gmail.com>
2020-05-30 15:24:11 -0500

    Modificacion_Classe_Cliente

--- src/main/java/ec/edu/ups/coopjam/model/Cliente.java
+++ src/main/java/ec/edu/ups/coopjam/model/Cliente.java
@@ -1,5 +1,82 @@
 package ec.edu.ups.coopjam.model;
 
-public class Cliente {
+import java.beans.Transient;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente { 
+	
+	@Id
+	private String cedula; 
+	private String nombre; 
+	private String apellido; 
+	private String direccion; 
+	private String telefono1; 
+	private String telefono2; 
+	private String correo; 
+	private String usuario; 
+	private String clave;  
+	
+	public Cliente() {
+
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono1() {
+		return telefono1;
+	}
+	public void setTelefono1(String telefono1) {
+		this.telefono1 = telefono1;
+	}
+	public String getTelefono2() {
+		return telefono2;
+	}
+	public void setTelefono2(String telefono2) {
+		this.telefono2 = telefono2;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+	
 }
