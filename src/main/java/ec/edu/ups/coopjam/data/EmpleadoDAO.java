1177c87c71613a907d25f9ef7de6a313fee12a01
Malki Katari Yupanki Medina <malkiyupanki12@gmail.com>
2020-05-30 15:13:22 -0500

    Modificacion Clase Empleado

--- src/main/java/ec/edu/ups/coopjam/data/EmpleadoDAO.java
+++ src/main/java/ec/edu/ups/coopjam/data/EmpleadoDAO.java
@@ -2,6 +2,7 @@ package ec.edu.ups.coopjam.data;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -9,7 +10,7 @@ import javax.persistence.Query;
 import ec.edu.ups.coopjam.model.Empleado;
 
 
-
+@Stateless
 public class EmpleadoDAO {
 	
 	@PersistenceContext(name = "coopjamPersistenceUnit") 
