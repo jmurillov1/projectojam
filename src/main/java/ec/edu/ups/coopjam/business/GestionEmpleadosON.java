ee2aa1f0d345fd956c0a60988829a66f777bc5c8
Malki Katari Yupanki Medina <malkiyupanki12@gmail.com>
2020-05-31 12:41:16 -0500

    Modulo Empleado

---
+++ src/main/java/ec/edu/ups/coopjam/business/GestionEmpleadosON.java
@@ -0,0 +1,83 @@
+package ec.edu.ups.coopjam.business;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import ec.edu.ups.coopjam.data.EmpleadoDAO;
+import ec.edu.ups.coopjam.model.Empleado;
+
+@Stateless
+public class GestionEmpleadosON {
+
+	@Inject
+	EmpleadoDAO empleadoDAO;
+
+	public boolean validadorDeCedula(String cedula) throws Exception {
+		System.out.println(cedula + "    En Metodo ");
+		boolean cedulaCorrecta = false;
+		try {
+			if (cedula.length() == 10) // ConstantesApp.LongitudCedula
+			{
+				int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
+				if (tercerDigito < 6) {
+					int[] coefValCedula = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+					int verificador = Integer.parseInt(cedula.substring(9, 10));
+					int suma = 0;
+					int digito = 0;
+					for (int i = 0; i < (cedula.length() - 1); i++) {
+						digito = Integer.parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
+						suma += ((digito % 10) + (digito / 10));
+					}
+					if ((suma % 10 == 0) && (suma % 10 == verificador)) {
+						cedulaCorrecta = true;
+					} else if ((10 - (suma % 10)) == verificador) {
+						cedulaCorrecta = true;
+					} else {
+						cedulaCorrecta = false;
+					}
+				} else {
+					cedulaCorrecta = false;
+				}
+			} else {
+				cedulaCorrecta = false;
+			}
+		} catch (NumberFormatException nfe) {
+			cedulaCorrecta = false;
+		} catch (Exception err) {
+			cedulaCorrecta = false;
+			throw new Exception("Error cedula");
+		}
+		if (!cedulaCorrecta) {
+			return cedulaCorrecta;
+			//throw new Exception("Cedula Incorrecta");
+			
+		}
+		return cedulaCorrecta;
+	}
+
+	public void guardarEmpleado(Empleado empleado) throws SQLException, Exception {
+
+		if (!validadorDeCedula(empleado.getCedula())) {
+			throw new Exception("Cedula Incorrecta");
+		} else {
+
+			try {
+				empleadoDAO.insertarEmpleado(empleado);
+			} catch (Exception e) {
+				throw new Exception(e.toString());
+			}
+		}
+	}
+	
+	public Empleado usuarioRegistrado(String cedula) {
+		return empleadoDAO.obtenerEmpleado(cedula);
+	}
+	
+	public List<Empleado> listadoEmpleados(){
+		return empleadoDAO.obtener();
+	}
+
+}
--- src/main/java/ec/edu/ups/coopjam/view/EmpleadosBean.java
+++ src/main/java/ec/edu/ups/coopjam/view/EmpleadosBean.java
@@ -1,5 +1,139 @@
 package ec.edu.ups.coopjam.view;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+import ec.edu.ups.coopjam.business.GestionEmpleadosON;
+import ec.edu.ups.coopjam.model.Empleado;
+
+@ManagedBean
+@ViewScoped
 public class EmpleadosBean {
+
+	@Inject
+	private GestionEmpleadosON empleadoON;
+
+	private Empleado empleado;
+
+	private boolean cajero;
+
+	private boolean jefeCredito;
+
+	private boolean ced;
+	
+	private List<Empleado> listaEmpleados;
+
+	@PostConstruct
+	public void init() {
+		empleado = new Empleado();
+		loadData();
+	}
+
+	public GestionEmpleadosON getEmpleadoON() {
+		return empleadoON;
+	}
+
+	public void setEmpleadoON(GestionEmpleadosON empleadoON) {
+		this.empleadoON = empleadoON;
+	}
+
+	public Empleado getEmpleado() {
+		return empleado;
+	}
+
+	public void setEmpleado(Empleado empleado) {
+		this.empleado = empleado;
+	}
+
+	public boolean isCajero() {
+		return cajero;
+	}
+
+	public void setCajero(boolean cajero) {
+		this.cajero = cajero;
+	}
+
+	public boolean isJefeCredito() {
+		return jefeCredito;
+	}
+
+	public void setJefeCredito(boolean jefeCredito) {
+		this.jefeCredito = jefeCredito;
+	}
+
+	public boolean isCed() {
+		return ced;
+	}
+
+	public void setCed(boolean ced) {
+		this.ced = ced;
+	}
+	
+	
+	public List<Empleado> getListaEmpleados() {
+		return listaEmpleados;
+	}
+
+	public void setListaEmpleados(List<Empleado> listaEmpleados) {
+		this.listaEmpleados = listaEmpleados;
+	}
+
+	public String guardarDatos() {
+
+		System.out.println(this.empleado.getCedula() + "   " + this.empleado.getNombre() + cajero + jefeCredito);
+
+		try {
+			if (cajero == true) {
+				empleado.setRol("Cajero");
+				empleadoON.guardarEmpleado(empleado);
+			} else if (jefeCredito == true) {
+				empleado.setRol("JefeCredito");
+				empleadoON.guardarEmpleado(empleado);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Lista-Empleados";
+
+	}
+
+	public String valCedula() {
+		System.out.println("*-------*"+empleado.getCedula());
+		if (empleado.getCedula() != null) {
+			Empleado usuarioRegistrado = empleadoON.usuarioRegistrado(empleado.getCedula());
+			if (usuarioRegistrado != null) {
+				System.out.println("Registrado");
+				return "Empleado REGISTRADO";
+			}
+			try {
+				ced = empleadoON.validadorDeCedula(empleado.getCedula());
+				System.out.println(ced);
+				if (ced) {
+					return "Cedula Valida";
+				}else if(ced == false) {
+					return "Cedula Incorrecta";
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return " ";	
+	}
+	
+	public void loadData() {
+		listaEmpleados = empleadoON.listadoEmpleados();
+	}
 
 }
