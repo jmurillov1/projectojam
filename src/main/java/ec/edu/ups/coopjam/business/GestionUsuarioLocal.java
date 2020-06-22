ffb3b693f180260f81ea505abf8876240c7fd323
Malki Katari Yupanki Medina <malkiyupanki12@gmail.com>
2020-06-22 16:48:45 -0500

    Creacion SOAP

---
+++ src/main/java/ec/edu/ups/coopjam/business/GestionUsuarioLocal.java
@@ -0,0 +1,43 @@
+package ec.edu.ups.coopjam.business;
+
+import java.util.Date;
+import java.util.List;
+
+import ec.edu.ups.coopjam.model.Cliente;
+import ec.edu.ups.coopjam.model.CuentaDeAhorro;
+import ec.edu.ups.coopjam.model.Empleado;
+import ec.edu.ups.coopjam.model.SesionCliente;
+import ec.edu.ups.coopjam.model.Transaccion;
+
+public interface GestionUsuarioLocal {
+	public String generarNumeroDeCuenta();
+	public String getUsuario(String cedula, String nombre, String apellido);
+	public String getContraseña();
+	public void enviarCorreo(String destinatario, String asunto, String cuerpo);
+	public String fecha();
+	public String obtenerFecha(Date fecha);
+	public void guardarCliente(Cliente c);
+	public Cliente buscarCliente(String cedulaCliente);
+	public Cliente buscarClienteUsuarioContraseña(String usuario, String contraseña);
+	public void eliminarCliente(String cedulaCliente);
+	public void actualizarCliente(Cliente cliente);
+	public List<Cliente> listaClientes();
+	public void guardarCuentaDeAhorros(CuentaDeAhorro c);
+	public CuentaDeAhorro buscarCuentaDeAhorro(String numeroCuentaDeAhorro);
+	public CuentaDeAhorro buscarCuentaDeAhorroCliente(String cedulaCliente);
+	public void eliminarCuentaDeAhorro(String numeroCuentaDeAhorro);
+	public void actualizarCuentaDeAhorro(CuentaDeAhorro cuentaDeAhorro);
+	public List<CuentaDeAhorro> listaCuentaDeAhorros();
+	public void guardarSesion(SesionCliente sesionCliente);
+	public SesionCliente buscarSesionCliente(int codigoSesionCliente);
+	public List<SesionCliente> obtenerSesionesCliente(String cedulaCliente);
+	public boolean validadorDeCedula(String cedula);
+	public void guardarEmpleado(Empleado empleado);
+	public Empleado usuarioRegistrado(String cedula);
+	public List<Empleado> listadoEmpleados();
+	public Empleado usuario(String usuario,String contra);
+	public List<Transaccion> listadeTransacciones(String cedula);
+	public void guardarTransaccion(Transaccion t);
+	public List<Transaccion> obtenerTransaccionesFechaHora(String cedula, String fechaI, String fechaF);
+
+}
---
+++ src/main/java/ec/edu/ups/coopjam/servicios/ServicioSOAP.java
@@ -0,0 +1,12 @@
+package ec.edu.ups.coopjam.servicios;
+
+import javax.jws.WebService;
+
+@WebService
+public class ServicioSOAP {
+	
+	
+	
+	
+
+}
