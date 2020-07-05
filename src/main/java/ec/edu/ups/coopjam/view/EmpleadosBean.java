0a4f1fb762322824e1520427c36f0e44bde58a46
Malki Katari Yupanki Medina <malkiyupanki12@gmail.com>
2020-07-05 15:28:29 -0500

    Actualizacion SolicitudCredito

--- src/main/java/ec/edu/ups/coopjam/view/EmpleadosBean.java
+++ src/main/java/ec/edu/ups/coopjam/view/EmpleadosBean.java
@@ -1,5 +1,11 @@
 package ec.edu.ups.coopjam.view;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
@@ -7,7 +13,10 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 
 import ec.edu.ups.coopjam.business.GestionUsuarioLocal;
 import ec.edu.ups.coopjam.business.GestionUsuarios;
@@ -285,6 +294,59 @@ public class EmpleadosBean {
 		editable = false;
 		editabledos = true;
 	}
+	
+	public void ver(String tipo) throws IOException {
+		
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+
+		File file = File.createTempFile("archivoTemp", ".pdf");
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+			if (tipo.equalsIgnoreCase("cedula")) {
+				fos.write(solicitudDeCredito.getArCedula());
+			}else if (tipo.equalsIgnoreCase("planilla")) {
+				fos.write(solicitudDeCredito.getArPlanillaServicios());
+			}else if (tipo.equalsIgnoreCase("rol")) {
+				fos.write(solicitudDeCredito.getArRolDePagos());
+			}
+			
+		}
+		BufferedInputStream input = null;
+		BufferedOutputStream output = null;
+
+		try {
+			// Open file.
+			input = new BufferedInputStream(new FileInputStream(file), 10240);
+
+			// Init servlet response.
+			response.reset();
+			response.setHeader("Content-Type", "application/pdf");
+			response.setHeader("Content-Length", String.valueOf(file.length()));
+			response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+			output = new BufferedOutputStream(response.getOutputStream(), 10240);
+
+			// Write file contents to response.
+			byte[] buffer = new byte [10240];
+			int length;
+			while ((length = input.read(buffer)) > 0) {
+				output.write(buffer, 0, length);
+			}
+
+			// Finalize task.
+			output.flush();
+		} finally {
+
+		}
+
+		// Inform JSF that it doesn't need to handle response.
+		// This is very important, otherwise you will get the following exception in the
+		// logs:
+		// java.lang.IllegalStateException: Cannot forward after response has been
+		// committed.
+		facesContext.responseComplete();
+	}
+	
 	
 
 }
