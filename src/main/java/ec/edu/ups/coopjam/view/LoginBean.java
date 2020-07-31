c5de09eaff36753237a1c5df0086958b56202c1a
Malki Katari Yupanki Medina <malkiyupanki12@gmail.com>
2020-07-31 03:57:45 -0500

    Actualizacion Malki

--- src/main/java/ec/edu/ups/coopjam/view/LoginBean.java
+++ src/main/java/ec/edu/ups/coopjam/view/LoginBean.java
@@ -475,6 +475,7 @@ public class LoginBean {
 	   public String logout() {
 	        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 	        return "InicioUsuarios?faces-redirect=true";
+		  // return null;
 	        
 	    }
 	
